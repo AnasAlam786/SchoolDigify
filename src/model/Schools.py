c16abfabff958ef734aba8325790886538a05a80
Anas Alam <74352709+AnasAlam786@users.noreply.github.com>
2026-02-03 14:11:05 +0530

    Remove Classes column from Schools model

--- src/model/Schools.py
+++ src/model/Schools.py
@@ -17,7 +17,6 @@ class Schools(db.Model):
     Email = Column(Text, unique=True, nullable=True)
     Password = Column(Text, nullable=True)
     Manager = Column(Text, nullable=False)
-    Classes = Column(JSON, nullable=False)              # Not null in DB
     IP = Column(JSON, nullable=True)
     students_image_folder_id = Column(Text, nullable=True)  # Added students_image_folder_id as per DB
     school_heading_image = Column(Text, nullable=True)  # Added school_heading_image as per DB
